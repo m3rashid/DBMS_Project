00c0a7206bbf933be043eef6146fb408d8436598
anzal1 <you@example.com>
2022-02-11 12:07:50 +0530

    login page

---
+++ client/src/pages/Login/loginform.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./loginpage.css";
+import Login from "../../components/LoginForm/loginform";
+
+const Loginpage= () =>{
+return (
+        <main>        
+          <Login />
+        </main>
+);
+}
+
+export default Loginpage
