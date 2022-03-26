89b7b59eadfc13cf02cd45c400dc94cdd19fc840
Ayesha <fizaayesha696@gmail.com>
2022-03-27 00:00:02 +1200

    ayesha

--- src/handlers/comments.js
+++ src/handlers/comments.js
@@ -9,6 +9,41 @@ const { checkAuth } = require("../middlewares/jwt.auth");
 
 //
 
+
+
+
+//TRY AYESHA
+
+router.post("/addComments", checkAuth, async (req, res) => {
+    const {postId, userId, text} = req.body;
+    try {
+      if (!postId || !userId)
+        throw new Error("No postId or userId");
+  
+      const commentId = uuidv4();
+  
+      const db = await pool.getConnection();
+      const [comment, _] = await db.query(
+        "insert into Comment (commentId, text, userID, postID) values ( ?, ?, ?, ?)",
+        [commentId, text, userId, postId]
+      );
+      return res.status(200).json({
+        message: "Comment Added successfully",
+        comment,
+      });
+      db.release();
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: err.message,
+      });
+    }
+  });
+
+
+
+
+
 //
 
 module.exports = router;
