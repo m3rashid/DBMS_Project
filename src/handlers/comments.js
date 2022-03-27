eb5922d85c4c85dedcf876ae312a0a1e01f9f73e
Four <aimzfatima08@gmail.com>
2022-03-28 00:50:09 +0530

    delete comment left

--- src/handlers/comments.js
+++ src/handlers/comments.js
@@ -17,7 +17,7 @@ const { checkAuth } = require("../middlewares/jwt.auth");
 router.post("/addComments", checkAuth, async (req, res) => {
     const {postId, userId, text} = req.body;
     try {
-      if (!postId || !userId)
+      if (!postId)
         throw new Error("No postId or userId");
   
       const commentId = uuidv4();
@@ -39,10 +39,61 @@ router.post("/addComments", checkAuth, async (req, res) => {
     }
   });
 
+//deleteing comments
 
+//do we have to give access to admin to delete the comments?? 
 
+router.post("/delete", checkAuth, async (req, res) => {
+    const { postID , commentID, userID } = req.body;
+   
+    try {
+      if (!postID && !commentID && !userID) throw new Error("Post and Comment not found ");
+  
+      const db = await pool.getConnection();
+  
+      //deleting comment
+      const [, _] = await db.query("delete from Comments where  commentID = ?", 
+      [
+        postID, commentID, userID
+      ]);
+      db.release();
+  
+      return res.status(200).json({
+        message: "Comment Deleted successfully",
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: err.message,
+      });
+    }
+  });
 
+  ////////////
 
-//
+  router.post("/edit", checkAuth, async (req, res) => {
+    const { commentID,text, userID } = req.body;
+   
+    try {
+      if (!commentID && !userID) throw new Error("NO comment found ");
+  
+      const db = await pool.getConnection();
+  
+      //updating topic
+      const [, _] = await db.query("update Comments set text = ?, updatedAt = CURRENT_TIMESTAMP(3) where CommentID = ?", [
+        text,commentID,
+      ]);
+      db.release();
+  
+      return res.status(200).json({
+        message: "Comment Editted successfully",
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: err.message,
+      });
+    }
+  });
 
 module.exports = router;
