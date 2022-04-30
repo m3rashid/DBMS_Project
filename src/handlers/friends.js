c29066b26944020daf1f64889d26e0f6114e0446
haiderzm <haider1272002@gmail.com>
2022-04-30 21:12:50 +0530

    request denied func

--- src/handlers/friends.js
+++ src/handlers/friends.js
@@ -13,13 +13,41 @@ const acceptRequest = async (req, res) => {
     [fromID, toID]
   );
 
-  if (status === 0) {
-    status = 1;
+  if (status === 1) {
+    status = 2;
+    await db.query(
+      "update Friendship set status = ? WHERE fromID = ? AND toID = ?",
+      [status, fromID, toID]
+    );
+    db.release();
+    return res.status(200).json({ status: "Request accepted !" });
+  } else {
+    db.release();
+    return res.status(400).json({ status: "Something went wrong!" });
   }
-  await db.query(
-    "update Friendship set status = ? WHERE fromID = ? AND toID = ?",
-    [status, fromID, toID]
+};
+
+const denyRequest = async (req, res) => {
+  const toID = req.body.toID; // dont know from where to get this
+  const fromID = req.body.fromID; // dont know from where to get this
+
+  const db = await pool.getConnection();
+
+  const [status] = await db.query(
+    "Select status from Friendship WHERE fromID = ? AND toID = ?",
+    [fromID, toID]
   );
-  db.release();
-  return res.status(200).json({ status: "Request accepted !" });
+
+  if (status === 1) {
+    status = 0;
+    await db.query(
+      "update Friendship set status = ? WHERE fromID = ? AND toID = ?",
+      [status, fromID, toID]
+    );
+    db.release();
+    return res.status(200).json({ status: "Request accepted !" });
+  } else {
+    db.release();
+    return res.status(400).json({ status: "Something went wrong!" });
+  }
 };
