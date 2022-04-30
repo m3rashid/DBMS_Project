3b93fc0410a6eb7e82dd632c17213b7994aa920a
M0hammad-Kashif <iamkashif20@gmail.com>
2022-04-30 20:12:12 +0530

    sendReq & accept req added

--- src/handlers/friends.js
+++ src/handlers/friends.js
@@ -14,7 +14,19 @@ const sendRequest = async (req ,res) =>{
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   var sendDateTime = date+' '+time;
   
-  await db.query("INSERT INTO Friendship (friendshipID,fromID,toID,status,createdAt,updatedAt) VALUES (?,?,?,?,?,?)",[friendshipID,fromID,toID,0,sendDateTime,null]);
+  await db.query("INSERT INTO Friendship (friendshipID,fromID,toID,status,createdAt,updatedAt) VALUES (?,?,?,?,?,?)",[friendshipID,fromID,toID,1,sendDateTime,null]);       // 1 for requested
   db.release();
   return res.status(200).json({ message : "Friendship request sent successfully."})
+
 }
+
+const acceptRequest = async (req ,res) =>{
+  const toID = req.body.toID;  // dont know from where to get this
+  const fromID = null   // dont know from where to get this
+  const [user,_] = await db.query("Select * FROM Friendship WHERE fromID = ? AND toID = ? AND status = ?",[fromID,toID,1])
+
+
+}
+
+// Todo :
+  // 1. Update from --> A x-->B
