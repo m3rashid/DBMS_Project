879b045a0a01415bacfc7fecec2264c3d7fff073
Haider Zama <46081121+haiderzm@users.noreply.github.com>
2022-05-02 11:46:50 +0530

    Added deny request

--- src/handlers/friends.js
+++ src/handlers/friends.js
@@ -94,3 +94,28 @@ const unblockUser = async (req, res) => {
     return res.status(400).json({ message: "Something went wrong :( " });
   }
 };
+
+const denyRequest = async (req, res) => {
+  const toID = req.body.toID; // dont know from where to get this
+  const fromID = req.body.fromID; // dont know from where to get this
+
+  const db = await pool.getConnection();
+
+  const [status] = await db.query(
+    "Select status from Friendship WHERE fromID = ? AND toID = ?",
+    [fromID, toID]
+  );
+
+  if (status === 1) {
+    status = 0;
+    await db.query(
+      "update Friendship set status = ? WHERE fromID = ? AND toID = ?",
+      [status, fromID, toID]
+    );
+    db.release();
+    return res.status(200).json({ status: "Request accepted !" });
+  } else {
+    db.release();
+    return res.status(400).json({ status: "Something went wrong!" });
+  }
+};
